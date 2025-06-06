af12ede701bd1149dd36eedecbb004f2ad74744b
Колесник Сергей <00volchenok0013@mail.ru>
2025-06-06 15:55:20 +0300

    create forms.py; update views.py, urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
+from django.views.generic import ListView
+
+
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постров
+    """
+
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
 
 def post_list(request):
     post_list_total = Post.published.all()
@@ -15,8 +29,7 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, year, month, day, post
-):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
         status=Post.Status.PUBLISHED,
@@ -27,3 +40,14 @@ def post_detail(request, year, month, day, post
     )
 
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == "POST":
+        form = EmailPostForm(request.Post)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    return render(request, "blog/post/share.html", {"post": post, "form": form})
