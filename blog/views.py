bfdb7a3d55a142a3679bb9695e8b4f4751061c26
Колесник Сергей <00volchenok0013@mail.ru>
2025-06-10 15:52:37 +0300

    create comments

--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,13 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.conf import settings
-
+from django.views.decorators.http import require_POST
+from django.db.models import Count
 
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+
+from taggit.models import Tag
 
 
 class PostListView(ListView):
@@ -20,8 +23,13 @@ class PostListView(ListView):
     template_name = "blog/post/list.html"
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list_total = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list_total = post_list_total.filter(tags=tag)
     paginator = Paginator(post_list_total, 3)
     page_number = request.GET.get("page", 1)
     try:
@@ -30,7 +38,7 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(request, "blog/post/list.html", {"posts": posts, "tag": tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -43,7 +51,19 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
 
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by("-same_tags", "-publish")[:4]
+
+    return render(request, "blog/post/detail.html", {
+        "post": post,
+        "comments": comments,
+        "form": form,
+        "similar_posts": similar_posts,
+        })
 
 
 def post_share(request, post_id):
@@ -68,3 +88,22 @@ def post_share(request, post_id):
     return render(
         request, "blog/post/share.html", {"post": post, "form": form, "sent": sent}
     )
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, "blog/post/comment.html", {
+        "post": post,
+        "form": form,
+        "comment": comment
+    })
