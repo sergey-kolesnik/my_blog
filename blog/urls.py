7291e33f76b0df1b7f6451d1cf92fde185bce3cb
sergei_kolesnik <00volchenok0013@mail.ru>
2025-06-15 16:41:06 +0300

    update settings.py urls.py create docker-compose.yaml Dockerfile

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path("tag/<slug:tag_slug>", post_list, name="post_list_by_tag"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"),
     path("<int:post_id>/share/", post_share, name="post_share"),
-    path("<int:post_id>/comment/", post_comment, name="post_comment")
+    path("<int:post_id>/comment/", post_comment, name="post_comment"),
+
 
 ]
